318a8d36c7fafce8f1d705768c724c32d6105d2e
istruthy <daren.lunsford+istruthy@gmail.com>
2020-07-15 23:24:59 -0700

    dev: test api call

---
+++ functions/src/hello.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+exports.handler = (event, context, callback) => {
+  const path = event.path.replace(/\.netlify\/functions\/[^/]+/, '');
+  const segments = path.split('/').filter((e) => e);
+  const id = segments[1];
+  console.log(segments);
+  //413400573
+  axios
+    .get(`https://www.mofo.com/templates/content-people-api?cid=${id}`)
+    .then((res) => {
+      callback(null, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        statusCode: 200,
+        body: JSON.stringify(res.data),
+      });
+    })
+    .catch((err) => {
+      callback(err);
+    });
+};
